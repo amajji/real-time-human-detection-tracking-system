58acc6754469bd8012be175d7d78d0e4d8bf8c72
majjianass <anassmajji34@gmail.com>
2023-10-20 22:33:37 +0200

    add github actions workflows

--- src/app/app.py
+++ src/app/app.py
@@ -35,14 +35,14 @@ import torch
 from shutil import rmtree
 import sys
 from pathlib import Path
-from yolov5.utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
-from yolov5.utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements,
+from src.yolov5.utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreenshots, LoadStreams
+from src.yolov5.utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements,
                            colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer,
                            xyxy2xywh)
-from yolov5.utils.plots import Annotator, colors, save_one_box
-from yolov5.utils.torch_utils import select_device, smart_inference_mode
-from yolov5.models.common import DetectMultiBackend
+from src.yolov5.utils.plots import Annotator, colors, save_one_box
+from src.yolov5.utils.torch_utils import select_device, smart_inference_mode
+from src.yolov5.models.common import DetectMultiBackend
 
 import logging
 ##########################################################################################################################
