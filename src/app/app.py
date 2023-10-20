2f366330e187801e99a4d72ee33d7286203e15e9
majjianass <anassmajji34@gmail.com>
2023-10-21 00:10:55 +0200

    add github actions workflows

--- src/app/app.py
+++ src/app/app.py
@@ -83,10 +83,12 @@ print("model loeaded ... ")
 
 # FILE = Path(__file__).resolve()
 # ROOT = FILE.parents[0]  # YOLOv5 root directory
+"""
 ROOT = os.path.dirname(os.path.realpath('__file__'))
 if str(ROOT) not in sys.path:
         sys.path.append(str(ROOT))  # add ROOT to PATH
 ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
+"""
 
 
 def decision(image_imread, box_coord):
